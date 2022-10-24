68f03c72f9a62c85035c3d427f6f367a5941d800
john.wu <john.wu@tdsc.com.tw>
2022-10-24 17:15:23 +0800

    fix

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by VSCode.
+ */
+require_once './vendor/autoload.php';
+
+use AsiaEast\Test\Test;
+
+$Test = new Test();
+
+echo $Test->desc();
---
+++ src/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Test\Test;
+
+class Test
+{
+    public function desc()
+    {
+        echo "this is john, he know nothing.";
+    }
+}
